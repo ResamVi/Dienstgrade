7808ee6ddecc500639bcd571ac0ff4f4eaf7734c
Julien Midedji <Julien.Midedji@gmail.com>
2015-09-04 10:45:29 +0200

    Implemented menu

---
+++ Assets/Scripts/ButtonListener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonListener : MonoBehaviour {
+
+	public void StartGame(bool isEasy) {
+		if (isEasy)
+			Application.LoadLevel (1);
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+
+}
